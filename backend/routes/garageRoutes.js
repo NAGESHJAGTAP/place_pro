252a37136ecb4c6dd3b59cea21ad5afdc8732cf3
Nageshjagtap <nageshjagtap063@gmail.com>
2025-02-06 19:34:46 +0530

    Add routes for garage CRUD operations: Get, Post, Put, Patch, Delete

--- backend/routes/garageRoutes.js
+++ backend/routes/garageRoutes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const garageController = require('../Controllers/garageController');
 
-// GET: Retrieve all garages
 router.get('/garages', garageController.getAllGarages);
-
-// GET: Retrieve a specific garage by name
 router.get('/garages/:name', garageController.getGarageByName);
-
-// POST: Add a new garage
 router.post('/garages', garageController.addGarage);
+router.put('/garages/:name', garageController.updateGarage);
+router.patch('/garages/:name', garageController.updateGarageFields);
+router.delete('/garages/:name', garageController.deleteGarage);
 
 module.exports = router;
