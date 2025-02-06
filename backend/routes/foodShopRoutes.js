c86e7ba46c191e5ff4ad2d39b25cbf0ca0c2282f
Nageshjagtap <nageshjagtap063@gmail.com>
2025-02-06 23:50:31 +0530

    Added Express routes for PUT, PATCH, and DELETE operations on food shops

--- backend/routes/foodShopRoutes.js
+++ backend/routes/foodShopRoutes.js
@@ -5,5 +5,8 @@ const foodShopController = require('../Controllers/foodShopController');
 router.get('/food_shops', foodShopController.getAllFoodShops);
 router.get('/food_shops/:name', foodShopController.getFoodShopByName);
 router.post('/food_shops', foodShopController.addFoodShop);
+router.put('/food_shops/:name', foodShopController.updateFoodShop);
+router.patch('/food_shops/:name', foodShopController.updateFoodShopFields);
+router.delete('/food_shops/:name', foodShopController.deleteFoodShop);
 
 module.exports = router;
