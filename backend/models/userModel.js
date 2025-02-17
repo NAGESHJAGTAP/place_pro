430742667e2b3acf64d709953b7e3ee6aa7e4d75
Nageshjagtap <nageshjagtap063@gmail.com>
2025-02-17 23:14:13 +0530

    add the user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,15 @@
+const { getDB } = require('../config/db');
+
+const collectionName = 'users'; 
+
+const createUser = async (user) => {
+  const db = getDB();
+  return await db.collection(collectionName).insertOne(user);
+};
+
+const findUserByEmail = async (email) => {
+  const db = getDB();
+  return await db.collection(collectionName).findOne({ email });
+};
+
+module.exports = { createUser, findUserByEmail };
