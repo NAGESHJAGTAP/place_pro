a57187a837ebe42c136099dfa7cfff3a47c43860
Nageshjagtap <nageshjagtap063@gmail.com>
2025-02-23 20:40:12 +0530

    modify code db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect("mongodb+srv://test:test123@cluster0.kwbnz.mongodb.net/project", {
-//       serverSelectionTimeoutMS: 10000, 
-//       socketTimeoutMS: 45000, 
-//     });
-
-//     console.log("MongoDB Connected Successfully!");
-//   } catch (error) {
-//     console.error(" MongoDB Connection Error:", error.message);
-//     process.exit(1);  
-//   }
-// };
-
-// module.exports = { connectDB };
-
-
 const mongoose = require("mongoose");
 
 let db;
