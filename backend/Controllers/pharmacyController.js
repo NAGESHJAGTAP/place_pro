82403cac4d2f75a82aaeb92aa5fbd94fb7ffde7b
Nageshjagtap <nageshjagtap063@gmail.com>
2025-03-06 11:59:53 +0530

    remove the commit out code

--- backend/Controllers/pharmacyController.js
+++ backend/Controllers/pharmacyController.js
@@ -1,54 +1,9 @@
-// const Pharmacy = require("../models/pharmacyModel");
-
-// exports.addPharmacy = async (req, res) => {
-//   try {
-//     const { name, address, city, state, zip, phone, email, website, openingHours, services, latitude, longitude } = req.body;
-//     const image = req.file ? req.file.filename : null;
-
-//     const newPharmacy = new Pharmacy({
-//       name,
-//       address,
-//       city,
-//       state,
-//       zip,
-//       phone,
-//       email,
-//       website,
-//       openingHours,
-//       services: services ? services.split(",") : [],
-//       latitude,
-//       longitude,
-//      image: req.file ? req.file.filename : null,
-//     });
-
-//     await newPharmacy.save();
-//     res.status(201).json({ message: "Pharmacy added successfully!" });
-//   } catch (error) {
-//     console.error("Error adding pharmacy:", error);
-//     res.status(500).json({ message: "Failed to add pharmacy." });
-//   }
-// };
-
-// exports.getPharmacies = async (req, res) => {
-//   try {
-//     const pharmacies = await Pharmacy.find();
-//     res.json(pharmacies);
-//   } catch (error) {
-//     console.error("Error fetching pharmacies:", error);
-//     res.status(500).json({ message: "Server error." });
-//   }
-// };
-
-
-
-
-
 
 const Pharmacy = require("../models/pharmacyModel");
 
 exports.addPharmacy = async (req, res) => {
   try {
-    console.log("Received Data:", req.body); // Debugging
+    console.log("Received Data:", req.body); 
     
     const { name, address, city, state, zip, phone, email, website, openingHours, services, latitude, longitude } = req.body;
     const image = req.file ? req.file.filename : null;
